69f77bcb4ab30e2c77240dacf504144d1970fa4a
Natan Rocha <natan.rocha@codeandsoul.com>
2024-10-17 17:04:05 -0300

    feat: desktop filter layout developed

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,8 +2,23 @@ import { Product } from "./Product";
 
 const serverUrl = "http://localhost:5000";
 
+function toggleColorFilters() {
+  const hiddenCheckboxes = document.querySelectorAll('.hidden');
+  const toggleButton = document.getElementById('toggle-color-filters');
+
+  hiddenCheckboxes.forEach(checkbox => {
+    checkbox.classList.toggle('hidden');
+  });
+  if (toggleButton) {
+    toggleButton.style.display = 'none';
+  }
+}
+
 function main() {
   console.log(serverUrl);
+
+  // Adiciona o evento de clique ao texto de mostrar/esconder os filtros
+  document.getElementById('toggle-color-filters')?.addEventListener('click', toggleColorFilters);
 }
 
 document.addEventListener("DOMContentLoaded", main);
